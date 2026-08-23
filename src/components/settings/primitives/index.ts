a97b64bce165d8b9c61805492793722835b2feac
Thierry <tofunori@MacBookPro-de-Thierry.local>
2026-08-23 10:24:40 -0400

    auto: Ajouter pastille « Enregistré » aux paramètres

--- src/components/settings/primitives/index.ts
+++ src/components/settings/primitives/index.ts
@@ -2,3 +2,4 @@ export { Row } from "./Row";
 export { Group } from "./Group";
 export { Advanced } from "./Advanced";
 export { Toggle } from "./Toggle";
+export { SavedIndicator, useSavedFlash } from "./SavedIndicator";
